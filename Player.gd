205242a75d4f43390bd1623c411e01008ae9e0e3
Gabriel Pedrotti da Silva <gabrielps2003@gmail.com>
2022-05-06 23:23:51 -0300

    menu
    menu commit

--- Player.gd
+++ Player.gd
@@ -4,21 +4,21 @@ const UP = Vector2(0, -1)
 const GRAVITY = 20
 const SPEED = 200
 const JUMP_HEIGHT = -550
+
 var motion = Vector2()
 
 func _physics_process(delta):
-	
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
-	else:
+	else: 
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
-		
+	
 	motion = move_and_slide(motion, UP)
