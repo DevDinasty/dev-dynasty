29db78977b4ba1c58b2b3ae89ce12c48fbc4882d
Gabriel Pedrotti da Silva <gabrielps2003@gmail.com>
2022-06-29 21:40:40 -0300

    feat: created fac

--- Player.gd
+++ Player.gd
@@ -8,14 +8,22 @@ const JUMP_HEIGHT = -550
 var motion = Vector2()
 
 func _physics_process(delta):
-	motion.y += GRAVITY
+#	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = SPEED
 	else: 
 		motion.x = 0
+		motion.y = 0
+	
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene("res://src/Battle.tscn")
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
---
+++ menu/Control.gd
@@ -0,0 +1,23 @@
+extends Button
+
+func _on_Start_mouse_entered():
+	$Sprite.modulate.r = 0.7
+	$Sprite.modulate.g = 0.7
+	$Sprite.modulate.b = 0.7
+	
+	pass # Replace with function body.
+
+func _on_Start_mouse_exited():
+	$Sprite.modulate.r = 1
+	$Sprite.modulate.g = 1
+	$Sprite.modulate.b = 1
+	
+	pass # Replace with function body.
+
+
+func _on_Start_pressed():
+	$Sprite.modulate.r = 0.5
+	$Sprite.modulate.g = 0.5
+	$Sprite.modulate.b = 0.5
+	get_tree().change_scene("res://Fac/Fac.tscn")
+	pass # Replace with function body.
