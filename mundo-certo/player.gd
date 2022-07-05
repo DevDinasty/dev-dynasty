acf2155d6e45bddae0ffbe8be27721f087102810
Marco Antonio <79797841+marcofeliponi@users.noreply.github.com>
2022-07-05 20:25:37 -0300

    feat: novo mapa e player

---
+++ mundo-certo/player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+onready var animation: AnimationPlayer = get_node("Animation")
+
+var velocity: Vector2
+
+export(int) var speed
+
+func _physics_process(_delta: float) -> void:
+	move()
+	animate()
+
+func move() -> void:
+	var direction_vector: Vector2 = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+	
+	velocity = direction_vector * speed
+	velocity = move_and_slide(velocity)
+	
+func animate() -> void:
+	if velocity != Vector2.ZERO:
+		animation.play("Run")
+	else:
+		animation.play("Idle")
